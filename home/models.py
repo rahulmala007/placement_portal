802c19bfa3ba2b5a99e46f8cfcf1c3d5d05f8930
coolgoel <coolgoel18@gmail.com>
2019-10-31 22:41:22 +0530

    database

--- home/models.py
+++ home/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator,MinValueValidator
 
-class Branch(models.Model):
-	branchName = models.CharField(max_length=50, default="")
-	num= models.IntegerField(default=0,validators=[MinValueValidator(0)])
-
-	def __str__(self):
-		return self.branchName
 
 BRANCH_CHOICES = [
 	('CSE', 'Computer Science and Engineering'),
@@ -21,11 +15,24 @@ BRANCH_CHOICES = [
 	('BT','Biotechnology'),
 ]
 
+class Branch(models.Model):
+	branchName = models.CharField(
+		max_length=50, 
+		choices=BRANCH_CHOICES,
+		default='CSE'
+		)
+	num= models.IntegerField(default=0,validators=[MinValueValidator(0)])
+
+	def __str__(self):
+		return self.branchName
+
+
+
 class Student(models.Model):
 	name = models.CharField(max_length=200)
 
 	PROGRAM_CHOICES = [
-        ('BTech', 'Bachelors of Technology'),
+        ('BTech', 'Bachelor of Technology'),
         ('BDes', 'Bachelor of Design'),
         ('MTech', 'Master of Technology'),
         ('MDes', 'Master of Design'),
