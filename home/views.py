eb58ef96de49e4993d1638a4a8170cff2c31825f
Pranav Gupta <pranavgupta000@gmail.com>
2019-10-31 22:43:04 +0530

    added studnet list

--- home/views.py
+++ home/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
 def index(request):
 	return render(request, 'home/home.html')
+
+def studentsList(request):
+	students = Student.objects.all()
+	context = {
+		'students': students,
+	}
+
+	return render(request, 'home/studentsList.html', context)
+
+
