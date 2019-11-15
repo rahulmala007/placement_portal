d56f309a00952a12fd8d0426352bef86b4972ddb
Pranav <pranavgupta000@gmail.com>
2019-11-16 00:50:51 +0530

    search fix

--- static/home/ajaxStudent.js
+++ static/home/ajaxStudent.js
@@ -1,3 +1,13 @@
+$(document).ready(function() {
+  $(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+  });
+});
+
+
 $(function() {
 
     $('#searchStudent').keyup(function() {
